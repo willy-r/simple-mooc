7a0f1e89e7701a36df7ecf2302fff2b3bc6ef9a0
William Rodrigues <williamrodrigues2442@gmail.com>
2021-03-09 13:55:29 -0300

    added the lesson and material models to admin site

--- courses/utils.py
+++ courses/utils.py
@@ -4,5 +4,5 @@ def material_directory_path(instance, filename):
     The materials for a lesson will be saved in a directory with the
     name of the course.
     """
-    course_name = instance.lesson.course.replace(' ', '_')
+    course_name = instance.lesson.course.name.replace(' ', '_').lower()
     return f'courses/lessons/materials/{course_name}/{filename}'
