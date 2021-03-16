eb9f2cdc95d2b99308d35adc5505ae36d3429fa7
William Rodrigues <williamrodrigues2442@gmail.com>
2021-03-16 14:45:13 -0300

    added tests to core app

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from django.urls import reverse
+from django.test import SimpleTestCase
 
-# Create your tests here.
+
+class HomeViewTests(SimpleTestCase):
+
+    def test_view_url_accessible_at_desired_location(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('core:home'))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('core:home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+
+
+class ContactViewTests(SimpleTestCase):
+
+    def test_view_url_accessible_at_desired_location(self):
+        response = self.client.get('/contato/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('core:contact'))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('core:contact'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'contact.html')
